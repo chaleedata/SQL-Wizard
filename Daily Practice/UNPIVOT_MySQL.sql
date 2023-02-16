c032a5e22c242ca53441c4439a37418fa93ea229
chalee <chalee.chea@gmail.com>
2023-02-16 22:40:16 +0700

    first_version

---
+++ Daily Practice/UNPIVOT_MySQL.sql
@@ -0,0 +1,34 @@
+--In MySQL, there is no specific UNPIVOT operator, but you can achieve the same result using the combination of other SQL clauses, such as the "UNION" operator, "CROSS JOIN" operator, and "CASE" statement.
+
+Input: 
+Products table:
++------------+--------+--------+--------+
+| product_id | store1 | store2 | store3 |
++------------+--------+--------+--------+
+| 0          | 95     | 100    | 105    |
+| 1          | 70     | null   | 80     |
++------------+--------+--------+--------+
+
+Output: 
++------------+--------+-------+
+| product_id | store  | price |
++------------+--------+-------+
+| 0          | store1 | 95    |
+| 0          | store2 | 100   |
+| 0          | store3 | 105   |
+| 1          | store1 | 70    |
+| 1          | store3 | 80    |
++------------+--------+-------+
+
+
+SELECT product_id, 'store1' AS store, store1 AS price 
+FROM Products 
+WHERE store1 IS NOT NULL
+UNION 
+SELECT product_id, 'store2' AS store, store2 AS price 
+FROM Products 
+WHERE store2 IS NOT NULL
+UNION 
+SELECT product_id, 'store3' AS store, store3 AS price 
+FROM Products 
+WHERE store3 IS NOT NULL
